ef49e51f359c2c4cf37ccb1c7dc9b31a4062350d
Nuintun <nuintun@qq.com>
2014-10-09 17:54:39 +0800

    add encode error message

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,18 @@
-// 点击扩展按钮生成二维码
+var QRBox = $('#qr-box');
+
 chrome.tabs.getSelected(function(tab) {
-	$('#qrcode').QREncode({
+	QRBox.QREncode({
 		text: tab.url,
 		moduleSize: 3,
-		logo: 'images/qrlogo.ico'
+		logo: 'images/qrlogo.ico',
+		error: function (e){
+			var error = $('#error'),
+				message = e.errorCode === 2
+					? '链接地址过长，超过了二维码的存储上限，<br/>请使用短链接服务生成短链接后再生成二维码！'
+					: e.message;
+
+			QRBox.hide();
+			error.html(message).show();
+		}
 	});
 });
