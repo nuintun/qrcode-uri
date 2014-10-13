f8bfc4a63f89fb9d2620a381eef7094da5241d24
Nuintun <nuintun@qq.com>
2014-10-13 18:02:48 +0800

    split files

--- src/js/background.js
+++ src/js/background.js
@@ -1,3 +1,6 @@
-chrome.tabs.onUpdated.addListener(function (id){
-    chrome.pageAction.show(id);
+/**
+ * 监听标签更新事件并显示扩展按钮
+ */
+chrome.tabs.onUpdated.addListener(function (uuid){
+    chrome.pageAction.show(uuid);
 });
