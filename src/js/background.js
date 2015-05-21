685ea53ed71c79acb4d75e9cac0fdd26d19d3541
Nuintun <nuintun@qq.com>
2015-05-21 15:46:45 +0800

    adjust code style

--- src/js/background.js
+++ src/js/background.js
@@ -2,19 +2,19 @@
  * 获取初始化标签并显示pageAction按钮
  */
 chrome.tabs.getSelected(function (tab){
-    chrome.pageAction.show(tab.id);
+  chrome.pageAction.show(tab.id);
 });
 
 /**
  * 监听标签的选择变更事件并显示pageAction按钮
  */
 chrome.tabs.onSelectionChanged.addListener(function (uuid){
-    chrome.pageAction.show(uuid);
+  chrome.pageAction.show(uuid);
 });
 
 /**
  * 监听标签的状态更新事件并显示pageAction按钮
  */
 chrome.tabs.onUpdated.addListener(function (uuid){
-    chrome.pageAction.show(uuid);
+  chrome.pageAction.show(uuid);
 });
